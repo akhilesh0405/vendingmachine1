2133854afef1be968bce37459b1b751d37cfd7f9
akhilesh0405 <akhileshcowlessur54@gmail.com>
2025-10-14 22:33:11 +0400

    Update asgi.py

--- my_tennis_club/my_tennis_club/asgi.py
+++ my_tennis_club/my_tennis_club/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_tennis_club.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_tennis_club.my_tennis_club.settings')
 
 application = get_asgi_application()
+
